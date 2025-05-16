c2583f360a66d846cf73d204802945550f62836e
Shawn Badger <shawnb@blackwiresig.com>
2025-05-15 21:29:55 -0600

    Add custom.el

---
+++ custom.el
@@ -0,0 +1,16 @@
+;;; -*- lexical-binding: t -*-
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(initial-frame-alist '((fullscreen . maximized))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(telephone-line-evil-insert ((t (:inherit telephone-line-evil :background "#61afef" :foreground "#000000"))))
+ '(telephone-line-evil-normal ((t (:inherit telephone-line-evil :background "#98c379" :foreground "#000000"))))
+ '(telephone-line-evil-replace ((t (:inherit telephone-line-evil :background "#e06c75" :foreground "#000000"))))
+ '(telephone-line-evil-visual ((t (:inherit telephone-line-evil :background "#c678dd" :foreground "#000000")))))
